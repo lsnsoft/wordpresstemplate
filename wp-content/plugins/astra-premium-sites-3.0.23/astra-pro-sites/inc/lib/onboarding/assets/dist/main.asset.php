d6fef8c239606ecc327771e2e59395dccea41a9f
Tarun <tarun@lsnsoft.com>
2022-01-06 13:40:37 +0530

    updated plugins

---
+++ wp-content/plugins/astra-premium-sites-3.0.23/astra-pro-sites/inc/lib/onboarding/assets/dist/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-polyfill', 'wp-url'), 'version' => '47dc57f6f675629ca02934d76dab272f');
